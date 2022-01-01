5c3c55553c0a89ef381dce877cfd3736de73ffa3
Divuzki <divuzki@gmail.com>
2022-01-01 17:27:32 +0100

    1st Jun, 2022

--- skitte/utils.py
+++ skitte/utils.py
@@ -119,8 +119,8 @@ def chat_unique_slug_generator(instance, new_slug=None):
 
 def make_text_bg(self):
     memfile = BytesIO()
-    caption = rot13_encrypt(self.caption)
-    imgpath = f'skitte-images\\contentBackgroundImage\\skt_cation\\{self.user.username}\\skt_cation+{truncate_string(caption)}_image.png'
+    caption = self.caption
+    imgpath = f'skitte-images\\contentBackgroundImage\\skt_cation\\{self.user.username}\\skt_cation+{rot13_encrypt(truncate_string(caption))}_image.png'
     imgpath = f"{imgpath.replace(' ', '0')}.png"
     wrapper = textwrap.TextWrapper(width=35)
     word_list = wrapper.wrap(text=caption)
