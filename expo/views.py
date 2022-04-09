4e21e2a4684dc5d2b2dc2836266b64c33e5ff7fb
Divuzki <divuzki@gmail.com>
2022-04-09 20:55:08 +0100

    Added Passcode In Ex App

--- expo/views.py
+++ expo/views.py
@@ -1,19 +1,32 @@
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, ListView, CreateView
 from django.db.models import Q
 
-from .models import Document, Chapter, Textz
+from .models import Document, Chapter, Textz, PassCode as Pass
 
 # Home page view
 
 
-class Home(ListView):
-    model = Chapter
-    template_name = 'p/home.html'
-    context_object_name = 'chapters'
+def home_view(request):
+    passcode = request.COOKIES.get('pass_code', False)
+    if passcode and not passcode == "":
+        qs = Pass.objects.filter(passcode=passcode).first()
+        if not qs is None:
+            if qs.used_count <= 2:
+                qs = Chapter.objects.all()
+                return render(request, "p/home.html", {"chapters": qs})
+            else:
+                res = redirect("/ex/pchekr/?e=your+code+has+exceeded+its+usage")
+                # res.set_cookie('pass_code', False, max_age=0)
+                # res.set_cookie('pass-code-used', False, max_age=0)
+                return res
+        else:
+            return redirect("/ex/pchekr/")
+    else:
+        return redirect("/ex/pchekr/")
+
 
 # Uploading document page view
-
-
 class Upload(CreateView):
     model = Document
     template_name = 'p/upload.html'
@@ -40,16 +53,22 @@ class Search(TemplateView):
 
     # Check if query returned anything and merge all result lists
     def get_queryset(self, *agrs, **kwargs):
-        if not self.get_query():
-            return None
-        else:
-            results = []
-            for q in self.get_query():
-                if results == []:
-                    results = self.get_result(q)
+        passcode = self.request.COOKIES.get('pass_code', False)
+        if passcode:
+            qs = Pass.objects.filter(passcode=passcode).first()
+            if not qs is None and qs.used_count <= 2:
+                if not self.get_query():
+                    return None
                 else:
-                    results = list(set(results) & set(self.get_result(q)))
-            return results
+                    results = []
+                    for q in self.get_query():
+                        if results == []:
+                            results = self.get_result(q)
+                        else:
+                            results = list(set(results) & set(self.get_result(q)))
+            else:
+                return redirect("/ex/pchekr/")
+        return results
 
     # Send all necessary variables to page render
     def get_context_data(self, **kwargs):
@@ -57,3 +76,49 @@ class Search(TemplateView):
         context['query'] = self.request.GET.get('query') or ''
         context['results'] = self.get_queryset()
         return context
+
+
+def passcode_checker(request):
+    res = render(request, "p/pcheck.html")
+    if request.method == "POST":
+        passcode = request.COOKIES.get('pass_code', False)
+        code = request.POST.get('pass_code').strip()
+        if passcode and not passcode == "":
+            code = passcode
+        if code == "" or code == None:
+            res = render(request, "p/pcheck.html",
+                         {"error": "You need to enter your pass guy! 🤦🏾‍♂️"})
+        else:
+            qs = Pass.objects.filter(passcode=code).first()
+            if not qs is None:
+                if qs.used_count >= 3:
+                    res = render(request, "p/pcheck.html", {
+                                    "error": "Sorry, You have exceeded your passcode usage 🙆🏾‍♂️ <strong style='color: brown;'>JUST GO AND BUY ANOTHER ONE</strong>"})
+                else:
+                    qs.used_count = qs.used_count + 1
+                    # replace redirect with HttpResponse or render
+                    res = redirect("/ex/")
+                    res.set_cookie('pass_code', qs.passcode, max_age=7200)
+                    res.set_cookie('pass-code-used',
+                                   qs.used_count, max_age=7200)
+                    qs.save()
+            else:
+                try_error = request.COOKIES.get('try-error', False)
+                if try_error:
+                    error = "JUST REST 🥱. Your Code don't exists"
+                else:
+                    error = "Oga, your code nor dey exists! 🤣, <b>GO AND BUY</b>"
+                res = render(request, "p/pcheck.html",
+                             {"error": error})
+                res.set_cookie('try-error', True, max_age=120*120)
+
+        return res
+    else:
+        return res
+
+# remove all cookies        
+def end_session(request):
+    res = redirect("/ex/pchekr/")
+    res.delete_cookie('pass_code')
+    res.delete_cookie('pass-code-used')
+    return res
